18819a430d1ea9d863f5bc3bdf193dd7737b121e
Andrew8633 <155861926+Andrew8633@users.noreply.github.com>
2024-01-12 22:37:23 +0900

    Add files via upload
    stylesheets and js

---
+++ testers.js
@@ -0,0 +1,267 @@
+function test1() {
+     document.getElementById("aa").style.cssText = "color: black; ";
+    document.getElementById("ab").style.cssText = "color: white; ";  
+}    
+    function test2() {
+     document.getElementById("aa").style.cssText = "color: white; ";
+    document.getElementById("ab").style.cssText = "color: black; ";  
+}    
+
+    function test3() {
+     document.getElementById("ba").style.cssText = "color: black; ";
+    document.getElementById("bb").style.cssText = "color: white; ";  
+}    
+    function test4() {
+     document.getElementById("ba").style.cssText = "color: white; ";
+    document.getElementById("bb").style.cssText = "color: black; ";  
+}    
+    
+        function test5() {
+     document.getElementById("ca").style.cssText = "color: black; ";
+    document.getElementById("cb").style.cssText = "color: white; ";  
+}    
+    function test6() {
+     document.getElementById("ca").style.cssText = "color: white; ";
+    document.getElementById("cb").style.cssText = "color: black; ";  
+}    
+    
+     function test7() {
+     document.getElementById("da").style.cssText = "color: black; ";
+    document.getElementById("db").style.cssText = "color: white; ";  
+}    
+    function test8() {
+     document.getElementById("da").style.cssText = "color: white; ";
+    document.getElementById("db").style.cssText = "color: black; ";  
+}    
+    
+    function test9() {
+     document.getElementById("ea").style.cssText = "color: black; ";
+    document.getElementById("eb").style.cssText = "color: white; ";  
+}    
+    function test10() {
+     document.getElementById("ea").style.cssText = "color: white; ";
+    document.getElementById("eb").style.cssText = "color: black; ";  
+}    
+    
+     function test11() {
+     document.getElementById("fa").style.cssText = "color: black; ";
+    document.getElementById("fb").style.cssText = "color: white; ";  
+}    
+    function test12() {
+     document.getElementById("fa").style.cssText = "color: white; ";
+    document.getElementById("fb").style.cssText = "color: black; ";  
+}    
+    
+     function test13() {
+     document.getElementById("ga").style.cssText = "color: black; ";
+    document.getElementById("gb").style.cssText = "color: white; ";  
+}    
+    function test14() {
+     document.getElementById("ga").style.cssText = "color: white; ";
+    document.getElementById("gb").style.cssText = "color: black; ";  
+}    
+     function test15() {
+     document.getElementById("ha").style.cssText = "color: black; ";
+    document.getElementById("hb").style.cssText = "color: white; ";  
+}    
+    function test16() {
+     document.getElementById("ha").style.cssText = "color: white; ";
+    document.getElementById("hb").style.cssText = "color: black; ";  
+}    
+     function test17() {
+     document.getElementById("ia").style.cssText = "color: black; ";
+    document.getElementById("ib").style.cssText = "color: white; ";  
+}    
+    function test18() {
+     document.getElementById("ia").style.cssText = "color: white; ";
+    document.getElementById("ib").style.cssText = "color: black; ";  
+}    
+     function test19() {
+     document.getElementById("ja").style.cssText = "color: black; ";
+    document.getElementById("jb").style.cssText = "color: white; ";  
+}    
+    function test20() {
+     document.getElementById("ja").style.cssText = "color: white; ";
+    document.getElementById("jb").style.cssText = "color: black; ";  
+}   
+
+    //question 1
+    
+function checkq1a() {
+        document.getElementById("c1a").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c1b").style.cssText = "background-color: white" ;
+        document.getElementById("c1c").style.cssText = "background-color: white" ;
+        document.getElementById("q1p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q1a").innerHTML = document.getElementById("c1a").innerHTML;
+    }
+function checkq1b() {
+        document.getElementById("c1a").style.cssText = "background-color: white" ;
+        document.getElementById("c1b").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c1c").style.cssText = "background-color: white" ;
+        document.getElementById("q1p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q1a").innerHTML = document.getElementById("c1b").innerHTML;
+    }
+function checkq1c() {
+        document.getElementById("c1a").style.cssText = "background-color: white" ;
+        document.getElementById("c1b").style.cssText = "background-color: white" ;
+        document.getElementById("c1c").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("q1p").style.cssText = "background-color: white" ;
+    
+        document.getElementById("q1a").innerHTML = document.getElementById("c1c").innerHTML;
+    }
+    
+    //question 2
+    
+function checkq2a() {
+        document.getElementById("c2a").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c2b").style.cssText = "background-color: white" ;
+        document.getElementById("c2c").style.cssText = "background-color: white" ;
+        document.getElementById("q2p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q2a").innerHTML = document.getElementById("c2a").innerHTML;
+    }
+function checkq2b() {
+        document.getElementById("c2a").style.cssText = "background-color: white" ;
+        document.getElementById("c2b").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c2c").style.cssText = "background-color: white" ;
+        document.getElementById("q2p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q2a").innerHTML = document.getElementById("c2b").innerHTML;
+    }
+function checkq2c() {
+        document.getElementById("c2a").style.cssText = "background-color: white" ;
+        document.getElementById("c2b").style.cssText = "background-color: white" ;
+        document.getElementById("c2c").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("q2p").style.cssText = "background-color: white" ;
+    
+        document.getElementById("q2a").innerHTML = document.getElementById("c2c").innerHTML;
+    }
+
+    //question 3
+function checkq3a() {
+        document.getElementById("c3a").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c3b").style.cssText = "background-color: white" ;
+        document.getElementById("c3c").style.cssText = "background-color: white" ;
+        document.getElementById("q3p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q3a").innerHTML = document.getElementById("c3a").innerHTML;
+    }
+function checkq3b() {
+        document.getElementById("c3a").style.cssText = "background-color: white" ;
+        document.getElementById("c3b").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c3c").style.cssText = "background-color: white" ;
+        document.getElementById("q3p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q3a").innerHTML = document.getElementById("c3b").innerHTML;
+    }
+function checkq3c() {
+        document.getElementById("c3a").style.cssText = "background-color: white" ;
+        document.getElementById("c3b").style.cssText = "background-color: white" ;
+        document.getElementById("c3c").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("q3p").style.cssText = "background-color: white" ;
+    
+        document.getElementById("q3a").innerHTML = document.getElementById("c3c").innerHTML;
+}
+    //question 4
+    
+function checkq4a() {
+        document.getElementById("c4a").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c4b").style.cssText = "background-color: white" ;
+        document.getElementById("c4c").style.cssText = "background-color: white" ;
+        document.getElementById("q4p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q4a").innerHTML = document.getElementById("c4a").innerHTML;
+    }
+function checkq4b() {
+        document.getElementById("c4a").style.cssText = "background-color: white" ;
+        document.getElementById("c4b").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c4c").style.cssText = "background-color: white" ;
+        document.getElementById("q4p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q4a").innerHTML = document.getElementById("c4b").innerHTML;
+    }
+function checkq4c() {
+        document.getElementById("c4a").style.cssText = "background-color: white" ;
+        document.getElementById("c4b").style.cssText = "background-color: white" ;
+        document.getElementById("c4c").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("q4p").style.cssText = "background-color: white" ;
+    
+        document.getElementById("q4a").innerHTML = document.getElementById("c4c").innerHTML;
+}
+        //question 5
+    
+function checkq5a() {
+        document.getElementById("c5a").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c5b").style.cssText = "background-color: white" ;
+        document.getElementById("c5c").style.cssText = "background-color: white" ;
+        document.getElementById("q5p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q5a").innerHTML = document.getElementById("c5a").innerHTML;
+    }
+function checkq5b() {
+        document.getElementById("c5a").style.cssText = "background-color: white" ;
+        document.getElementById("c5b").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("c5c").style.cssText = "background-color: white" ;
+        document.getElementById("q5p").style.cssText = "background-color: white" ;
+        
+        document.getElementById("q5a").innerHTML = document.getElementById("c5b").innerHTML;
+    }
+function checkq5c() {
+        document.getElementById("c5a").style.cssText = "background-color: white" ;
+        document.getElementById("c5b").style.cssText = "background-color: white" ;
+        document.getElementById("c5c").style.cssText = "background-color: cornsilk" ;
+        document.getElementById("q5p").style.cssText = "background-color: white" ;
+    
+        document.getElementById("q5a").innerHTML = document.getElementById("c5c").innerHTML;
+}
+    
+
+function checkAll() {
+    var skor = 0;
+   
+    if(document.getElementById("q1").innerHTML == document.getElementById("q1a").innerHTML) {
+        document.getElementById("q1p").style.cssText = "background-color: chartreuse";
+        skor += 1;
+    
+    } else {document.getElementById("q1p").style.cssText = "background-color: lightcoral"}
+    
+    if(document.getElementById("q2").innerHTML == document.getElementById("q2a").innerHTML) {
+        document.getElementById("q2p").style.cssText = "background-color: chartreuse";
+        skor += 1;
+    
+    } else {document.getElementById("q2p").style.cssText = "background-color: lightcoral"}
+    
+    if(document.getElementById("q3").innerHTML == document.getElementById("q3a").innerHTML) {
+        document.getElementById("q3p").style.cssText = "background-color: chartreuse";
+        skor += 1;
+    
+    } else {document.getElementById("q3p").style.cssText = "background-color: lightcoral"}
+    
+    if(document.getElementById("q4").innerHTML == document.getElementById("q4a").innerHTML) {
+        document.getElementById("q4p").style.cssText = "background-color: chartreuse";
+        skor += 1;
+    
+    } else {document.getElementById("q4p").style.cssText = "background-color: lightcoral"}
+    
+    if(document.getElementById("q5").innerHTML == document.getElementById("q5a").innerHTML) {
+        document.getElementById("q5p").style.cssText = "background-color: chartreuse";
+        skor += 1;
+    
+    } else {document.getElementById("q5p").style.cssText = "background-color: lightcoral"}
+    
+    
+    let message = "You got " + skor + "/5 correct!"
+    document.getElementById("text").innerHTML = message;
+    on();
+}    
+
+function on() {
+document.getElementById("overlay").style.display = "block";
+    window.scrollBy(0, -5000);
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
